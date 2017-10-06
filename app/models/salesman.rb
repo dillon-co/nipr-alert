18f70d8fad186c775fb3d7c37716260242872f48
Dillon <dilloncortez@gmail.com>
2017-10-06 15:42:46 -0600

    fixed array not as hash bug

--- app/models/salesman.rb
+++ app/models/salesman.rb
@@ -160,15 +160,19 @@ class Salesman < ApplicationRecord
     #    Nokogiri::XML(f)
     doc_hash = Hash.from_xml(doc)
     doc_hash.first.last["SCB_Report_Body"]["SCB_Producer"].each do |a|
-      agent = self.find_by(npn: a["National_Producer_Number"])
-      if agent.present?
-        agent.update(first_name: a["Name_Birth"]["First_Name"].titleize,
-                    last_name: a["Name_Birth"]["First_Name"].titleize,
-                    agent_site: a["Address"].first["City"].titleize,
-                    home_work_location_city: a["Address"].first["City"].titleize)
-        self.update_batch_agent_state_data(a, agent)
-      else
-         self.create_agent_with_data(a)
+      begin
+        agent = self.find_by(npn: a["National_Producer_Number"])
+        if agent.present?
+          agent.update(first_name: a["Name_Birth"]["First_Name"].titleize,
+                      last_name: a["Name_Birth"]["First_Name"].titleize,
+                      agent_site: a["Address"].first["City"].titleize,
+                      home_work_location_city: a["Address"].first["City"].titleize)
+          self.update_batch_agent_state_data(a, agent)
+        else
+           self.create_agent_with_data(a)
+        end
+      rescue => e
+        next
       end
     end
   end
@@ -210,11 +214,11 @@ class Salesman < ApplicationRecord
                                 appont_renewal_date: appoint["Renewal_Date"]
                                 )
         end
-      end  
+      end
     end
   end
 
-  def create_agent_with_data(agent_data)
+  def self.create_agent_with_data(agent_data)
     a = self.create(first_name: agent_data["Name_Birth"]["First_Name"].titleize,
                  last_name: agent_data["Name_Birth"]["First_Name"].titleize,
                  agent_site: agent_data["Address"].first["City"].titleize,
