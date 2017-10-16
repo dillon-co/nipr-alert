771568be3d49702c5d60e731a4288f30e54c34bc
Dillon <dilloncortez@gmail.com>
2017-10-16 10:59:18 -0600

    fixed view bug and string bug

--- app/models/salesman.rb
+++ app/models/salesman.rb
@@ -161,7 +161,7 @@ class Salesman < ApplicationRecord
     doc_hash = Hash.from_xml(doc)
     doc_hash.first.last["SCB_Report_Body"]["SCB_Producer"].each do |a|
       # agent = self.turn_array_to_hash(agent)
-      agent = self.find_by(npn: a["National_Producer_Number"]).update!(first_name: a["Name_Birth"]["First_Name"].titleize,
+      agent = self.find_or_create_by!(npn: a["National_Producer_Number"]).update!(first_name: a["Name_Birth"]["First_Name"].titleize,
                   last_name: a["Name_Birth"]["Last_Name"].titleize,
                   agent_site: a["Address"].first["City"].titleize,
                   home_work_location_city: a["Address"].first["City"].titleize)
