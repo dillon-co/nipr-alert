702e214bf2ec4f667dc01ebd6e6855376fc31ead
Dillon <dilloncortez@gmail.com>
2017-10-16 08:10:39 -0600

    fixed view bug and string bug

--- app/controllers/salesmen_controller.rb
+++ app/controllers/salesmen_controller.rb
@@ -31,7 +31,7 @@ class SalesmenController < ApplicationController
     @salesman_last = @salesman.last_name.present? ? @salesman.last_name : @salesman.family_name
     @licensed_states = @salesman.states.all.compact
     @active_licenses = @licensed_states.map { |st| st.licenses.where(active: 'Y')}
-    @active_licenses_states_names = @active_licenses.map {|l| l.state.name }
+    @active_licenses_states_names = @active_licenses != [] ? @active_licenses.map {|l| l.state.name } : @active_licenses = []
     @appointed_states = @salesman.states.includes(:appointments).map{|s| s if s.appointments.count > 0 }.compact
     @non_appointed_states = @salesman.states.includes(:appointments).map{|s| s if s.appointments.count < 1 }.compact
     @expired_states = @salesman.states.includes(:licenses).where('date_expire_license < ?', Time.now).references(:licenses)
