61650dcd9180b22ea8618a44831c2b23f39baa9b
Dillon <dilloncortez@gmail.com>
2017-10-24 09:44:29 -0600

    Updated agent site

--- app/controllers/salesmen_controller.rb
+++ app/controllers/salesmen_controller.rb
@@ -42,7 +42,7 @@ class SalesmenController < ApplicationController
     @non_licensed_states = all_states_names - @all_salesman_states
     @licensed_states_names = @licensed_states.map(&:name)
     @appointed_states_names = @appointed_states.map(&:name)
-    @jit_states = sites_with_just_in_time_states#.select { |s| @licensed_states_names.include?(s)}
+    @jit_states = jit_states#.select { |s| @licensed_states_names.include?(s)}
     @can_sell_states = [@appointed_states_names, @jit_states].flatten.uniq.compact
     @check_or_naw = @needed_states - @can_sell_states
     @non_sellable_states_names = [@expired_states.compact.map(&:name), @non_appointed_states.compact.map(&:name)]
