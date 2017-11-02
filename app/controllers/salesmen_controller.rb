71465d8dc8931bc28fd052060d392a6bc777fa03
Dillon <dilloncortez@gmail.com>
2017-11-02 14:35:28 -0600

    Fixed trailing end

--- app/controllers/salesmen_controller.rb
+++ app/controllers/salesmen_controller.rb
@@ -31,15 +31,7 @@ class SalesmenController < ApplicationController
     @salesman_last = @salesman.last_name.present? ? @salesman.last_name : @salesman.family_name
 
     @check_or_naw = @salesman.array_of_states_needed#@needed_states - @can_sell_states
-    @licensed_states = @salesman.states.includes(:licenses).all.map do |s|
-      if s.licenses.last.present? && s.licenses.last.date_expire_license.present?
-        unless s.licenses.last.date_expire_license < Date.today
-          s
-        end
-      else
-        s
-      end
-    end
+    @licensed_states = @salesman.states.where(licensed: true)
     @active_licenses = @licensed_states.map { |st| st.licenses.where(active: 'Yes')}
     @active_licenses_states_names = @active_licenses.count > 0 ? @active_licenses.flatten.map {|l| l.state.name } : @active_licenses = []
     @appointed_states = @salesman.states.where(appointed: true)
